03e047492914798f5b9488bc22e88ef22a6fc825
Allen Anselmo <aransel@zai.com>
2024-07-31 08:36:34 -0400

    Latest next.js, making default / the clientonly example, and switching off reactStrictMode as it apparently breaks rc-dock

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+import { DockLayoutClient } from './_components/dock-layout-client';
+import { DockLayoutProvider } from './_providers/dock-layout-context';
+
+export default function Home() {
+  return <DockLayoutProvider><DockLayoutClient /></DockLayoutProvider>;
+}
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-/** @type {import("next").NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // causes all pages to render twice, which is good for shaking out problems, but sucks for debugging
+  reactStrictMode: false, // Recommended for the `pages` directory, default in `app`.
+};
+
+module.exports = nextConfig;
